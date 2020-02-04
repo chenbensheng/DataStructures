192720fce2f739795fd79998bdca9f3a62bb21dd
chenbensheng <chenbensheng@vipkid.com.cn>
2020-02-04 22:17:50 +0800

    数据结构及LeetCode

---
+++ src/main/java/com/app/Y.java
@@ -0,0 +1,15 @@
+package com.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Y {
+
+    @Autowired
+    Z z;
+
+    public Y(){
+        System.out.println("init y");
+    }
+}
